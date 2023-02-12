6b6d4f8dbab9d38ed576babdf7c23a6f5aeae832
MurilloDiana <dianamurillo.467@gmail.com>
2023-02-12 00:48:32 -0400

    sensor proximidad

--- app/src/main/java/com/example/puppyeyes/MainActivity.java
+++ app/src/main/java/com/example/puppyeyes/MainActivity.java
@@ -2,13 +2,60 @@ package com.example.puppyeyes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+SensorManager sensorManager;
+Sensor sensor;
+SensorEventListener sensorEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor=sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        if(sensor== null) finish();
+        sensorEventListener=new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if(sensorEvent.values[0]<sensor.getMaximumRange()){
+                    Toast.makeText(MainActivity.this, "holiwis", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "chaucito", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+
+        };
+        start();
+    }
+    public void start(){
+        sensorManager.registerListener(sensorEventListener,sensor, 2000*1000);
+    }
+    public void stop(){
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+
+    @Override
+    protected void onPause() {
+        stop();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        start();
+        super.onResume();
     }
 }
